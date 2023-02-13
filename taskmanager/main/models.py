dc54cf967f94ffc09faecca402cea1b9ffc7e529
Irina Yushina <missis.iuschina777@gmail.com>
2023-02-13 15:59:54 +0300

    delete_old_name

--- taskmanager/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Buyer(models.Model):
-    username = models.CharField('Имя покупателя', max_length=15),
-    usernumber = models.PositiveIntegerField('Номер получателя')
-
-    def __str__(self):
-        return self.username
